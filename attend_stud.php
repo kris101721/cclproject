c47de1f13bed5bfe5bd72da069cbe93e319e6b4c
kris101721 <98094951+kris101721@users.noreply.github.com>
2023-04-21 02:19:55 +0530

    Update attend_stud.php

--- attend_stud.php
+++ attend_stud.php
@@ -49,8 +49,15 @@
         </tr>
        
 <?php
-    include("connect.php");
-    error_reporting(0);
+    require "database/config.php";
+
+$conn = mysqli_init();
+mysqli_ssl_set($conn,NULL,NULL,$sslcert, NULL, NULL);
+if(!mysqli_real_connect($conn, $host, $dbusername, $dbpassword, $dbname, 3306, MYSQLI_CLIENT_SSL)){
+            die('Failed to connect to MySQL: '.mysqli_connect_error());
+        }else{
+
+}
     
     session_start();
     $uname = $_SESSION['uname'];
