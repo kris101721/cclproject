be59b77471521d730fab44a213e4bee1d859939e
kris101721 <98094951+kris101721@users.noreply.github.com>
2023-04-20 20:59:34 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,14 +1,18 @@
 <?php 
 
-$host = "localhost";
-$dbusername = "root";
-$dbpassword = "";
-$dbname = "attsys-database";
+$host = "ccl-project-server.mysql.database.azure.com";
+$dbusername = "kristen123";
+$dbpassword = "Pass@123";
+$dbname = "cclprojectdb";
   
 // Create connection
-$conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
+$conn = mysqli_init();
+mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+mysqli_real_connect($conn, "ccl-project-server.mysql.database.azure.com", "kristen123", $dbpassword, $dbname, 3306, MYSQLI_CLIENT_SSL);
 
 
 
 ?>
+
+
 
