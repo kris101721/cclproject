55a5f945e7eec63e957b524209fd937bc0e4c309
kris101721 <98094951+kris101721@users.noreply.github.com>
2023-04-21 01:16:04 +0530

    Update login.php

--- login.php
+++ login.php
@@ -7,7 +7,7 @@ require "database/config.php";
 
 $conn = mysqli_init();
 mysqli_ssl_set($conn,NULL,NULL,$sslcert, NULL, NULL);
-if(!mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, MYSQLI_CLIENT_SSL)){
+if(!mysqli_real_connect($conn, $host, $dbusername, $dbpassword, $dbname, 3306, MYSQLI_CLIENT_SSL)){
             die('Failed to connect to MySQL: '.mysqli_connect_error());
         }
 
