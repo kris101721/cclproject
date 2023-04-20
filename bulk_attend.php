1de61d47ea6c984511f9636782ec604654e91e27
kris101721 <98094951+kris101721@users.noreply.github.com>
2023-04-21 03:34:36 +0530

    Update bulk_attend.php

--- bulk_attend.php
+++ bulk_attend.php
@@ -1,5 +1,13 @@
 <?php
-	 include("connect.php");
+	 require "database/config.php";
+
+$conn = mysqli_init();
+mysqli_ssl_set($conn,NULL,NULL,$sslcert, NULL, NULL);
+if(!mysqli_real_connect($conn, $host, $dbusername, $dbpassword, $dbname, 3306, MYSQLI_CLIENT_SSL)){
+            die('Failed to connect to MySQL: '.mysqli_connect_error());
+        }else{
+
+}
 	//error_reporting(0);
 	session_start();
 	$date = $_POST['date'];
