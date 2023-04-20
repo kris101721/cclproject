0e30da540b4894c4782573455d4872e3343ca35c
kris101721 <98094951+kris101721@users.noreply.github.com>
2023-04-20 21:27:59 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -7,8 +7,14 @@ $dbname = "cclprojectdb";
   
 // Create connection
 
-$conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
-
+$conn = mysqli_init();
+mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+mysqli_real_connect($conn, "ccl-project-server.mysql.database.azure.com", "kristen123", $dbpassword, $dbname, 3306, MYSQLI_CLIENT_SSL);
+echo"
+<html>
+<body> Magic happens here         </body>
+</html>
+";
 ?>
 
 
